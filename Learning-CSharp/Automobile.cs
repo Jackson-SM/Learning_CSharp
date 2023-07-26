1ba3ec6849067192bad27040fcfc9661a06cce0c
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-07-26 20:49:46 -0300

    Iniciando uma classe básica no C#

---
+++ Learning-CSharp/Automobile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp
+{
+    internal class Automobile
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int ManufacturingDate {get; set;}
+
+        public void Acelerar()
+        {
+            Console.WriteLine("O Automóvel está acelerando!");
+        }
+        public void Frear()
+        {
+            Console.WriteLine("O Automóvel está freando!");
+        }
+    }
+}
